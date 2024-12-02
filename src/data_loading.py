89001ac228253d7b1ef73c43a32356dc9fe82fc8
sem-k32 <kirill.semkin32@mail.ru>
2024-12-02 20:02:31 +0300

    Project structure fixes

--- src/data_loading.py
+++ src/data_loading.py
@@ -20,7 +20,7 @@ def get_ds_infos(data_dir: str):
         data_dir += "/"
 
     dss = pd.read_csv(data_dir + "data_subjects_info.csv")
-    print("[INFO] -- Data subjects' information is imported.")
+    # print("[INFO] -- Data subjects' information is imported.")
     
     return dss
 
@@ -72,7 +72,7 @@ def creat_time_series(data_dir: str, dt_list, act_labels, trial_codes, mode="mag
         
     ds_list = get_ds_infos(data_dir)
     
-    print("[INFO] -- Creating Time-Series")
+    # print("[INFO] -- Creating Time-Series")
     for sub_id in ds_list["code"]:
         for act_id, act in enumerate(act_labels):
             for trial in trial_codes[act_id]:
